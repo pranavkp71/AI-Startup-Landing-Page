075695c91df3822bc51dff3e9ec7cebdfce6fb11
pranav <pranavkp170@gmail.com>
2025-05-29 12:10:19 +0530

    Create features section and add the page demo and features

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,9 @@
+"use client";
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import productImage from '@/assets/product-image.png';
+import Image from 'next/image';
+
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +32,27 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return <section className="py-20">
+    <div className="container">
+      <h2 className="text-5xl font-medium text-center tracking-tighter">Elevate your SEO efforts</h2>
+      <p className="text-wwhite/70 text-lg tracking-tight text-center mt-5">From small startups to large enterprises, our AI-driven tool has
+        revolutionized the way business approach SEO.
+      </p>
+      <div className="mt-10">
+        {tabs.map(tab => (
+          <div 
+            key={tab.title} 
+            className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center"
+          >
+            <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+            <DotLottiePlayer src={tab.icon} className="h-5 w-5" autoplay/>
+            </div>
+            <div>{tab.title}</div>
+            {tab.isNew && <div>new</div>}
+          </div>
+        ))}
+      </div>
+      <Image src={productImage} alt="product image" />
+    </div>
+  </section>;
 };
